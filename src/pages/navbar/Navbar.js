92957c78fb6f3840825918f48ee1657112b2eee8
Monise da Silva <monisesilva91@hotmail.com>
2024-12-18 15:53:20 -0300

    Melhorias no códigoo

--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="cabecalho">
             <div className="cabecalho-logo">Sites e Sistemas</div>
-            <nav className="cabecalho-menu">
+            <button className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </button>
+            <nav className={`cabecalho-menu ${menuOpen ? "open" : ""}`}>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#sobre">Sobre</a></li>
