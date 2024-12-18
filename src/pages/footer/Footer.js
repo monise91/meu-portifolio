2bfafd78677add36d9b7d0c71bbd76467ec769b5
Monise da Silva <monisesilva91@hotmail.com>
2024-12-18 15:11:27 -0300

    Melhorias no códigoo

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer className="footer">
-            <p>© 2024 Site e Sistema. Todos os direitos reservados.</p>
+            <p>© 2024 Site Sistema. Todos os direitos reservados.</p>
             <div className="footer-links">
                 <ul>
                     <li><a href="#home">Home</a></li>
