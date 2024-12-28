40fa2153f0de60c247aab46b5fcfb845e9f3f3b0
Monise da Silva <monisesilva91@hotmail.com>
2024-12-28 20:48:25 -0300

    Melhorias footer

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -5,16 +5,16 @@ function Footer() {
     return (
         <footer className="footer">
             <p>© 2024 Site e Sistema. Todos os direitos reservados.</p>
-            <div className="footer-links">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#sobre">Sobre</a></li>
-                    <li><a href="#servicos">Serviços</a></li>
-                    <li><a href="#projetos">Projetos</a></li>
-                    <li><a href="#competencia">Competências</a></li>
-                    <li><a href="#contato">Contato</a></li>
-                </ul>
-            </div>
+            {/*<div className="footer-links">*/}
+            {/*    <ul>*/}
+            {/*        <li><a href="#home">Home</a></li>*/}
+            {/*        <li><a href="#sobre">Sobre</a></li>*/}
+            {/*        <li><a href="#servicos">Serviços</a></li>*/}
+            {/*        <li><a href="#projetos">Projetos</a></li>*/}
+            {/*        <li><a href="#competencia">Competências</a></li>*/}
+            {/*        <li><a href="#contato">Contato</a></li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
         </footer>
     );
 }
